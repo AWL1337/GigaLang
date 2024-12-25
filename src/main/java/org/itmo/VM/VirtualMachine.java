80578291645d0db3881501c2fca90e14427ce50c
DmitriiPl <Plighig@mail.ru>
2024-12-25 10:52:02 +0300

    resolving Instruction

--- src/main/java/org/itmo/VM/VirtualMachine.java
+++ src/main/java/org/itmo/VM/VirtualMachine.java
@@ -1,14 +1,25 @@
 package org.itmo.VM;
 
 import org.itmo.VM.instructions.Instruction;
+import org.itmo.VM.memory.Manager;
 
 import java.util.List;
 
 
 public class VirtualMachine {
-
+    private final Manager manager = new Manager();
 
     public void interpret(List<Instruction> instructions) {
 
     }
+
+    private void execute(Instruction instruction) {
+        switch (instruction.getType()) {
+            case ADD:
+
+        }
+
+    }
+
+    private void handleAdd() {}
 }
--- src/main/java/org/itmo/VM/instructions/Instruction.java
+++ src/main/java/org/itmo/VM/instructions/Instruction.java
@@ -1,9 +1,11 @@
 package org.itmo.VM.instructions;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
+@Data
 @RequiredArgsConstructor
 public class Instruction {
     private final InstructionType type;
