680014bf63d5ab1135a322c153b5bc4de1a874f5
DmitriiPl <Plighig@mail.ru>
2025-01-18 12:19:39 +0300

    add executor

--- src/main/java/org/itmo/Main.java
+++ src/main/java/org/itmo/Main.java
@@ -1,29 +1,12 @@
 package org.itmo;
 
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
-import org.itmo.VM.instructions.Instruction;
-import org.itmo.antlr.GigaLangLexer;
-import org.itmo.antlr.GigaLangParser;
-import org.itmo.generator.ByteCodeGenerator;
-
-import java.util.List;
-
-
 public class Main {
     public static void main(String[] args) {
-        String input = "if (a > 0) { var b = 3\n }\n";
-        GigaLangLexer lexer = new GigaLangLexer(CharStreams.fromString(input));
-        GigaLangParser parser = new GigaLangParser(new CommonTokenStream(lexer));
-        ParseTree tree = parser.program();
 
-        ByteCodeGenerator generator = new ByteCodeGenerator();
-        generator.visit(tree);
+        String code = "a = 5;";
 
-        List<Instruction> instructions = generator.getInstructions();
-        for (Instruction instruction : instructions) {
-            System.out.println(instruction);
-        }
+        var codeExecutor = new GigaExecutor();
+
+        codeExecutor.executeCode(code);
     }
 }
